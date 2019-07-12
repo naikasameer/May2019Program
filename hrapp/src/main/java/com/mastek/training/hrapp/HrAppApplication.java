231f12437f2b83389af8a1c34087f1ba4f1032c8
naikasameer <sameer.naik3@mastek.com>
2019-07-12 14:22:50 +0100

    Spring Bean APplication Object and Test cases Created

--- hrapp/src/main/java/com/mastek/training/hrapp/HrAppApplication.java
+++ hrapp/src/main/java/com/mastek/training/hrapp/HrAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HrAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Test");
 		SpringApplication.run(HrAppApplication.class, args);
 	}
 
--- hrapp/src/test/java/com/mastek/training/hrapp/HrAppApplicationTests.java
+++ hrapp/src/test/java/com/mastek/training/hrapp/HrAppApplicationTests.java
@@ -2,15 +2,43 @@ package com.mastek.training.hrapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mastek.training.hrapp.apis.EmployeeService;
+import com.mastek.training.hrapp.entities.Employee;
+
+// Initialize the JUnit TEst with Spring Boot Application Environment
+// Spring Boot Test: used to test Spring APplicationContext 
+// with all the test cases idenitfied
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HrAppApplicationTests {
+	
+	// scan in memory all the components and provide the 
+	// best match object in the component
+	
+	@Autowired
+	EmployeeService empService;
 
+	@Autowired
+	Employee emp;
+	
 	@Test
-	public void contextLoads() {
+	public void addEmployeeUsingService() {
+		empService.registerEmployee(emp);
 	}
 
+	
+	@Test
+	public void simpleTest() {
+		System.out.println("System Test Executed");
+	}
+	
 }
+
+
+
+
